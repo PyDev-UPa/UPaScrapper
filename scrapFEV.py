61de4d1e4ae641f0fea4413219a5c1b814b551f8
kwakuman <kwakuman@pm.me>
2020-02-18 15:31:29 +0100

    added bs4 to requirements, created list of all the options from the soup

--- scrapFEV.py
+++ scrapFEV.py
@@ -2,3 +2,13 @@
 
 '''scrapFEV.py dowloands webpage https://www.upce.cz/en/foreign-education-verification
 and creates a list containing countries and groups from the html code for further processing'''
+
+import requests, bs4
+
+fevUrl = 'https://www.upce.cz/en/foreign-education-verification'
+
+sourcePage = requests.get(fevUrl)
+sourceSoup = bs4.BeautifulSoup(sourcePage.text, 'html.parser')
+allLines = sourceSoup.select('option')
+
+#TODO get rid of none country items in the list
