93841eea5af3fac3bda0e5fc463facc4c01cd5c9
kwakuman <kwakuman@pm.me>
2020-02-18 12:07:26 +0100

    First commit

---
+++ scrapFEV.py
@@ -0,0 +1,4 @@
+#!usr/bin/python
+
+'''scrapFEV.py dowloands webpage https://www.upce.cz/en/foreign-education-verification
+and creates a list containing countries and groups from the html code for further processing'''
