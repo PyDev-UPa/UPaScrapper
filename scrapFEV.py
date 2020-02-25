4af999fc30c4844087d1fccdb69733068d955882
Srb Ondrej <onsr3560@upce.cz>
2020-02-25 14:48:05 +0100

    changed shebang to work on windows

--- scrapFEV.py
+++ scrapFEV.py
@@ -1,4 +1,4 @@
-#!usr/bin/python
+#! python
 
 '''scrapFEV.py dowloands webpage https://www.upce.cz/en/foreign-education-verification
 and creates a list containing countries and groups from the html code for further processing'''
